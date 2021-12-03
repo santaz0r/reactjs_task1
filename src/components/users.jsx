a1bcad00db3743636db54e63d5d58783e9d1efec
VictorM <neuron96@mail.ru>
2021-12-03 22:45:59 +0500

    fix render table

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -32,63 +32,63 @@ const Users = () => {
       );
 
     if (users.length === 0) {
-      const table = document.querySelector(".table");
+      // const table = document.querySelector(".table");
 
       return (
-        (table.style.display = "none"),
-        (
-          <span className="badge bg-danger">
-            Никто с тобой не тусанет сегодня
-          </span>
-        )
+        // (table.style.display = "none"),
+        <span className="badge bg-danger">
+          Никто с тобой не тусанет сегодня
+        </span>
       );
     }
   };
   return (
     <>
       {handleChangeText()}
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Имя</th>
-            <th scope="col">Качества</th>
-            <th scope="col">Профессия</th>
-            <th scope="col">Встретился, раз</th>
-            <th scope="col">Оценка</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => {
-            return (
-              <tr key={user._id}>
-                <td>{user.name}</td>
-                <td>
-                  {user.qualities.map((q) => (
-                    <span
-                      key={q._id}
-                      className={"badge bg-" + q.color + " m-1"}
+      {users.length !== 0 && (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Профессия</th>
+              <th scope="col">Встретился, раз</th>
+              <th scope="col">Оценка</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => {
+              return (
+                <tr key={user._id}>
+                  <td>{user.name}</td>
+                  <td>
+                    {user.qualities.map((q) => (
+                      <span
+                        key={q._id}
+                        className={"badge bg-" + q.color + " m-1"}
+                      >
+                        {q.name}
+                      </span>
+                    ))}
+                  </td>
+                  <td>{user.profession.name}</td>
+                  <td>{user.completedMeetings}</td>
+                  <td>{user.rate} / 5</td>
+                  <td>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => handleDelete(user._id)}
                     >
-                      {q.name}
-                    </span>
-                  ))}
-                </td>
-                <td>{user.profession.name}</td>
-                <td>{user.completedMeetings}</td>
-                <td>{user.rate} / 5</td>
-                <td>
-                  <button
-                    className="btn btn-danger"
-                    onClick={() => handleDelete(user._id)}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </>
   );
 };
